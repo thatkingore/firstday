847e994fc532bcb7e12b3f9474fff2868d34dbde
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-01 23:54:56 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+print("         ^")
+print("        /|")
+print("       / |")
+print("      /  |")
+print("     /   |")
+print("    /    |")
+print("   /     |")
+print("  /      |")
+print(" /_______|")
+
+character_name = "Tom"
+character_age = "50"
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+
+character_name = "John"
+character_age = "35"
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
