603d395323df4ad3962427ff4c0f6561ffad09b1
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-14 00:19:57 +0100

    Update okayletsgetserious.py

--- okayletsgetserious.py
+++ okayletsgetserious.py
@@ -10,3 +10,32 @@ result = cube(4)
 
 print(cube(3))
 print(result)
+
+hungry = False
+if hungry:
+    print("Get some brekky :)")
+
+cloudy = True
+
+if cloudy:
+    print("Bring an umbrella")
+else:
+    print("Sunglasses babe")
+
+if hungry or cloudy:
+    print("What is going on today?")
+else:
+    print("You're good baby girl, you're good")
+
+if hungry and cloudy:
+    print("Very very weird")
+else:
+    print("Don't even worry about it")
+
+meat = False
+if hungry and meat:
+    print("Can I have steak please")
+elif hungry and not(meat):
+    print("Hmm how about spaghetti & meatballs")
+else:
+    print("Oh a salad!")
