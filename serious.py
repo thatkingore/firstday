2ec2720cbc01dbc3ce875ddabf9947c9d80a0305
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-13 22:38:38 +0100

    Add files via upload

---
+++ serious.py
@@ -0,0 +1,12 @@
+def say_hi(name, age):
+    print("Oh hello " + name + ", you are " + str(age))
+
+say_hi("ore", 23)
+say_hi("twinnie", 22)
+
+def cube(num):
+    return num*num*num
+result = cube(4)
+
+print(cube(3))
+print(result)
