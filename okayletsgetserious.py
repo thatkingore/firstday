c2cd1be4d6a2b69c46c63ecff2eaf5cb5e9802a4
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-23 22:31:36 +0100

    Update okayletsgetserious.py

--- okayletsgetserious.py
+++ okayletsgetserious.py
@@ -39,3 +39,15 @@ elif hungry and not(steak):
     print("Hmm how about spaghetti & meatballs")
 else:
     print("Oh a salad!")
+
+# Function to compare int using IF statements
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(4, 3, 1))
