8de6bf19c20cb8a3c1f5828134a47102712f327c
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-10-25 16:38:13 +0100

    Update okayletsgetserious.py

--- okayletsgetserious.py
+++ okayletsgetserious.py
@@ -88,3 +88,28 @@ for index in range(5):
         print("First Iteration")
     else:
         print("Not First")
+
+# FOR loop and range of numbers to create an exponent function
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(2, 3))
+
+# 2D Lists and Nested FOR loops
+
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+print(number_grid[2][1])
+
+for row in number_grid:
+    for col in row:
+        print(col)
