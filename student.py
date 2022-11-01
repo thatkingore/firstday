3b11049139b98ad54e901dfdcd7530e170d7d093
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-01 21:25:30 +0000

    Add files via upload

---
+++ student.py
@@ -0,0 +1,7 @@
+class student:
+
+    def __init__(self, name, degree, grade, is_on_probation):
+        self.name = name
+        self.degree = degree
+        self.grade = grade
+        self.is_on_probation = is_on_probation
