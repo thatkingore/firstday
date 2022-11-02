04b1e97ff308484b546e7195a8e68b8490e41ec0
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-02 23:54:33 +0000

    Add files via upload

---
+++ multiple choice quiz.py
@@ -0,0 +1,24 @@
+
+from question import Question
+
+question_prompts = [
+    "What colour are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What colour are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What colour are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b"),
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("Accuracy = " + str(score) + "/" + str(len(questions)))
+
+run_test(questions)
