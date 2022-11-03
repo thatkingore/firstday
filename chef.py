42a9eea19c061e3e269e9b8352f939a1ddd0a800
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-03 21:45:47 +0000

    Add files via upload

---
+++ chef.py
@@ -0,0 +1,10 @@
+class chef:
+
+    def make_chicken(self):
+        print("Chef made chicken.")
+
+    def make_salad(self):
+        print("Chef arranged a salad.")
+
+    def make_special_dish(self):
+        print("Chef made lasagne!")
---
+++ world_class_chef.py
@@ -0,0 +1,8 @@
+from chef import chef
+
+class world_class_chef(chef):
+
+    def make_special_dish(self):
+        print("Chef made orange chicken.")
+    def make_fried_rice(self):
+        print("Chef made fried rice, yum.")
