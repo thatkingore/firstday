17240c7e8ca6233c250018ab972d5dbb5efac54f
thatkingore <60559907+thatkingore@users.noreply.github.com>
2022-11-03 21:29:58 +0000

    Update student.py

--- student.py
+++ student.py
@@ -5,3 +5,9 @@ class student:
         self.degree = degree
         self.grade = grade
         self.is_on_probation = is_on_probation
+    
+    def first_class(self):
+        if self.grade >= 70:
+            return True
+        else:
+            return False
